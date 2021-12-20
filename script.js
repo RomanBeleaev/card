220eebffa9231c3c05b9896269e9699a2157fd31
RomanBeleaev <92945554+RomanBeleaev@users.noreply.github.com>
2021-12-20 13:53:20 +0200

    try to push date

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const numberIn = document.querySelector('#number')
 const user = document.querySelector('#name')
 const year = document.querySelector('#year')
 const month = document.querySelector('#month')
+const date = document.querySelector('#date')
 const cvv = document.querySelector('#cvv')
 //
 card.addEventListener('click', ()=>{
@@ -15,4 +16,13 @@ card.addEventListener('click', ()=>{
 if (numberIn == document.activeElement){
     document.querySelector('.focus-field').classList.add('num-focus')
 } else {document.querySelector('.focus-field').classList.remove('num-focus')
+}
+
+const pushYear = () =>{
+    let yearSelect = year.options[year.selectedIndex].text
+    date.textContent.replace(date.textContent.slice(3), yearSelect.slice(2))
+}
+const pushMonth = () =>{
+    let monthSelect = month.options[month.selectedIndex].text
+    date.textContent.replace(date.textContent.slice(0,2), monthSelect)
 }
